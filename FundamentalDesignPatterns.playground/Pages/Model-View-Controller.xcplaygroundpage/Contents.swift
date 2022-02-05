81e64f37be7e60f90eb6341e39b80710402b23ec
Gospodi <gospodijazzet@gmail.com>
2022-02-05 15:43:14 +0300

    добавлена диаграмма и описание паттерна (MVC)

--- FundamentalDesignPatterns.playground/Pages/Model-View-Controller.xcplaygroundpage/Contents.swift
+++ FundamentalDesignPatterns.playground/Pages/Model-View-Controller.xcplaygroundpage/Contents.swift
@@ -1,7 +1,17 @@
-//: [Previous](@previous)
-
-import Foundation
-
-var greeting = "Hello, playground"
-
-//: [Next](@next)
+/*:
+ [Previous](@previous)&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;[Next](@next)
+ 
+ # Model-view-controller (MVC)
+ - - - - - - - - - -
+ ![MVC Diagram](MVC_Diagram.png)
+ 
+ Паттерн модель-представление-контроллер (MVC) разделяет объекты на три типа: модели, представления и контроллеры.
+ 
+ **Models** хранят данные приложения. Обычно это структуры или простые классы.
+ 
+ **Views** отображают визуальные элементы и элементы управления на экране. Обычно они являются подклассами `UIView`.
+ 
+ **Controllers** координация между моделями и представлениями. Обычно они являются подклассами `UIViewController`.
+ 
+ ## Code Example
+ */
