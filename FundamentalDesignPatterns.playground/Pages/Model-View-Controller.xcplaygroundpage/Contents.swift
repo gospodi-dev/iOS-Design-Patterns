3bcb08a0a2d1b525f49c277e4dd67b7beaf698b8
Gospodi <gospodijazzet@gmail.com>
2022-02-05 16:02:55 +0300

    добавлен controller у которого есть сильная ссылка на view и model

--- FundamentalDesignPatterns.playground/Pages/Model-View-Controller.xcplaygroundpage/Contents.swift
+++ FundamentalDesignPatterns.playground/Pages/Model-View-Controller.xcplaygroundpage/Contents.swift
@@ -15,3 +15,31 @@
  
  ## Code Example
  */
+import UIKit
+
+// MARK: - Address
+public struct Address {
+  public var street: String
+  public var city: String
+  public var state: String
+  public var zipCode: String
+}
+
+// MARK: - AddressView
+public final class AddressView: UIView {
+  @IBOutlet public var streetTextField: UITextField!
+  @IBOutlet public var cityTextField: UITextField!
+  @IBOutlet public var stateTextField: UITextField!
+  @IBOutlet public var zipCodeTextField: UITextField!
+}
+
+// MARK: - AddressViewController
+public final class AddressViewController: UIViewController {
+
+  // MARK: - Properties
+  public var address: Address?
+  public var addressView: AddressView! {
+    guard isViewLoaded else { return nil }
+    return (view as! AddressView)
+  }
+}
